ff6da034145c02dd08b054e23e34576ec207a1ab
Sal <ssferrazza@users.noreply.github.com>
2024-12-11 12:52:06 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [
         .iOS(.v15),
         .macOS(.v12),
-        .tvOS(.v15)
+        .tvOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
