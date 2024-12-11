6b985a17c4d2ff19a1f6278041883c8e87c37110
Sal <ssferrazza@users.noreply.github.com>
2024-12-11 11:20:46 +0100

    Added tvOS support

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "RadixUI",
     platforms: [
         .iOS(.v15),
-        .macOS(.v12)
+        .macOS(.v12),
+        .tvOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
